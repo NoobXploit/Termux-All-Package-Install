82ed9d0bf83dabfd70be822787f2a2a1a3030610
NoobXploit <104293151+NoobXploit@users.noreply.github.com>
2022-05-04 15:53:11 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,227 @@
 from os import system
-system("clear")
 
-system("toilet -f  smblock   '    NoobXploit' |lolcat;apt update")
+system("clear ; toilet -f  smblock   '    NOOBXPLOIT' |lolcat ")
+
+
+option = """
+\033[1;32m[1]  Install Only Python Packages And Pip
+\033[1;32m[2]  Install All Packages
+\033[1;31m[3]  Banner
+"""
+print(option)
+
+user = int(input(" \033[1;34mChoose A Option >  "))
+
+if user == 1:
+  from os import system
+  system("clear ; toilet -f  smblock   '    NOOBXPLOIT' |lolcat")
+  try:
+    system("apt install python -y")
+  except:
+    print("Already Installed or Getting Error")
+  try:
+    system("apt install python2 -y")
+  except:
+    print("Already Installed or Getting Error")
+  try:
+    system("pip install pyngrok")
+  except:
+    print("Already Installed or Getting Error")
+  try:
+    system("pip install requests")
+  except:
+    print("Already Installed or Getting Error")
+  try:
+    system("pip install mechanize")
+  except:
+    print("Already Installed or Getting Error")
+  try:
+    system("pip2 install requests")
+  except:
+    print("Already Installed or Getting Error")
+  try:
+    system("pip2 install mechanize")
+  except:
+    print("Already Installed or Getting Error")
+  try:
+    system("pip2 install bs4")
+  except:
+    print("Already Installed or Getting Error")
+  try:
+    system("pip2 install futures")
+  except:
+    print("Already Installed or Getting Error")
+  try:
+    system("pip2 install lolcat")
+  except:
+    print("Already Installed or Getting Error")
+  try:
+    system("pip2 install mechanize")
+  except:
+    print("Already Installed or Getting Error")
+  try:
+    system("pip install npm")
+  except:
+    print("Already Installed or Getting Error")
+  try:
+    system("pip install lolcat")
+  except:
+    print("Already Installed or Getting Error")
+  try:
+    system("pip install bs4")
+  except:
+    print("Already Installed or Getting Error")
+elif user == 2:
+  from os import system
+  system("clear ; toilet -f  smblock   '    NOOBXPLOIT' |lolcat")
+  try:
+    system("apt update")
+  except:
+    print("Already Installed or Getting Some Error ")
+  try:
+    system("apt upgrade -y")
+  except:
+    print("Already Installed or Getting Error")
+  try:
+    system("pkg install toilet")
+  except:
+    print("Already Installed or Getting Error")
+  try:
+    system("pkg install php -y")
+  except:
+    print("Already Installed or Getting Error") 
+  try:
+    system("apt install python -y")
+  except:
+    print("Already Installed or Getting Error")
+  try:
+    system("apt install python2 -y")
+  except:
+    print("Already Installed or Getting Error")
+  try:
+    system("pip install pyngrok")
+  except:
+    print("Already Installed or Getting Error")
+  try:
+    system("pip install requests")
+  except:
+    print("Already Installed or Getting Error")
+  try:
+    system("pip install mechanize")
+  except:
+    print("Already Installed or Getting Error")
+  try:
+    system("pip2 install requests")
+  except:
+    print("Already Installed or Getting Error")
+  try:
+    system("pip2 install mechanize")
+  except:
+    print("Already Installed or Getting Error")
+  try:
+    system("pip2 install bs4")
+  except:
+    print("Already Installed or Getting Error")
+  try:
+    system("pip2 install futures")
+  except:
+    print("Already Installed or Getting Error")
+  try:
+    system("pip2 install lolcat")
+  except:
+    print("Already Installed or Getting Error")
+  try:
+    system("pip2 install mechanize")
+  except:
+    print("Already Installed or Getting Error")
+  try:
+    system("pip install npm")
+  except:
+    print("Already Installed or Getting Error")
+  try:
+    system("pip install lolcat")
+  except:
+    print("Already Installed or Getting Error")
+  try:
+    system("pip install bs4")
+  except:
+    print("Already Installed or Getting Error")
+  try:
+    system("apt install git -y")
+  except:
+    print("Already Installed or Getting Error") 
+  try:
+    system("apt install ruby -y")
+  except:
+    print("Already Installed or Getting Error")
+  try:
+    system("apt install nmap -y")
+  except:
+    print("Already Installed or Getting Error") 
+  try:
+    system("apt install bash -y")
+  except:
+    print("Already Installed or Getting Error")
+  try:
+    system("apt install clang -y")
+  except:
+    print("Already Installed or Getting Error")
+  try:
+    system("apt install jq -y")
+  except:
+    print("Already Installed or Getting Error")
+  try:
+    system("apt install macchanger -y")
+  except:
+    print("Already Installed or Getting Error")
+   
+  try:
+    system("apt install unzip -y")
+  except:
+    print("Already Installed or Getting Error") 
+   
+  try:
+    system("apt install zip -y")
+  except:
+    print("Already Installed or Getting Error")
+  try:
+    system("apt install tar -y")
+  except:
+    print("Already Installed or Getting Error")
+  try:
+    system("apt install curl -y")
+  except:
+    print("Already Installed or Getting Error")
+  try:
+    system("apt install nano -y")
+  except:
+    print("Already Installed or Getting Error")  
+  try:
+    system("apt install bmon -y")
+  except:
+    print("Already Installed or Getting Error") 
+  try:
+    system("apt install openssl")
+  except:
+    print("Already Installed or Getting Error")     
+  try:
+    system("apt install wcalc -y")
+  except:
+    print("Already Installed or Getting Error")      
+  try:
+    system("apt install wget -y")
+  except:
+    print("Already Installed or Getting Error")      
+  try:
+    system("apt install tor -y")
+  except:
+    print("Already Installed or Getting Error")      
+  try:
+    system("apt install php -y")
+  except:
+    print("Already Installed or Getting Error")
+elif user == 3:
+  print ("\033[1;33mWait For Update!")
+else:
+  print("\033[1;31mWrong Input")
+  exit()
