162195ac1765608e435adc9be3da61b891581db3
NoobXploit <104293151+NoobXploit@users.noreply.github.com>
2022-05-04 13:45:22 +0600

    Create tool.py

---
+++ tool.py
@@ -0,0 +1,72 @@
+from os import system
+echo
+echo
+apt update
+apt upgrade -y               
+apt install python -y          
+apt install python2 -y
+apt install ruby -y 
+apt install git -y
+
+ echo   "####################################"
+apt install php -y 
+apt install perl -y
+apt install nmap -y
+pkg install toilet
+
+                                                             
+ echo   "####################################"
+apt install bash -y
+apt install clang -y
+apt install jq -y
+apt install macchanger -y
+                                                                
+ echo   "####################################"
+apt install nano -y
+apt install curl -y
+apt install tar -y
+apt install zip -y
+
+
+                                                                   
+ echo   "####################################"
+apt install unzip -y
+apt install tor -y
+apt install wget -y
+
+
+                                                      
+ echo   "####################################"
+apt install wcalc -y 
+apt install openssl
+apt install bmon -y
+
+echo   "####################################"
+
+apt install python python3
+pip3 install phonenumbers
+pkg install git -y
+pkg install php -y
+pkg install openssh -y
+pip install pyngrok
+pip install requests
+pip install mechanize
+
+
+echo   "###################################"
+pip2 install requests
+
+pip2 install mechanize
+
+pip2 install requests
+
+pip2 install bs4
+
+pip2 install bs4 futures
+
+pip install bs4
+
+pip2 install lolcat
+pip2 install mechanize
+pip2 install requests bs4
+exit
